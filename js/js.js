a27e9cd9d9ac1314cc7478a3e383381617d54c5b
Pablo Perez <perez51160900@gmail.com>
2019-08-15 20:20:29 -0400

    initial commit

---
+++ js/js.js
@@ -0,0 +1,273 @@
+$(document).ready(function(){
+			$("#slide").hide();
+			$("#slide2").hide();
+			$("#slide3").hide();
+			$("#slide4").hide();
+
+			$('#la_sociedad').hover(
+				function(){
+				$("#slide").stop(true,true).slideDown('small');
+			},
+
+				function () {
+					$("#slide").stop(true,true).delay(3000).slideUp('medium');
+
+				}
+			);
+
+			$('#miembros').hover(
+				function(){
+				$("#slide2").stop(true,true).slideDown('small');
+			},
+
+				function () {
+					$("#slide2").stop(true,true).delay(2000).slideUp('medium');
+
+				}
+			);
+
+			$('#fundacion').hover(
+				function(){
+				$("#slide3").stop(true,true).slideDown('small');
+			},
+
+				function () {
+					$("#slide3").stop(true,true).delay(2000).slideUp('medium');
+
+				}
+			);
+
+			$('#revista').hover(
+				function(){
+				$("#slide4").stop(true,true).slideDown('small');
+			},
+
+				function () {
+					$("#slide4").stop(true,true).delay(2000).slideUp('medium');
+
+				}
+			);
+
+
+			$("#ln1").hover(function(){
+				$("#ln1").css('background-color','#D70505');
+			});
+			$("#ln2").hover(function(){
+				$("#ln2").css('background-color','#D70505');
+			});
+			$("#ln3").hover(function(){
+				$("#ln3").css('background-color','#D70505');
+			});
+			$("#ln4").hover(function(){
+				$("#ln4").css('background-color','#D70505');
+			});
+			$("#ln5").hover(function(){
+				$("#ln5").css('background-color','#D70505');
+			});
+			$("#ln6").hover(function(){
+				$("#ln6").css('background-color','#D70505');
+			});
+			$("#ln7").hover(function(){
+				$("#ln7").css('background-color','#D70505');
+			});
+			$("#ln8").hover(function(){
+				$("#ln8").css('background-color','#D70505');
+			});
+			$("#ln9").hover(function(){
+				$("#ln9").css('background-color','#D70505');
+			});
+			$("#ln10").hover(function(){
+				$("#ln10").css('background-color','#D70505');
+			});
+
+			$("#ln1").mouseleave(function(){
+				$("#ln1").css('background-color','#001063 ');
+			});
+			$("#ln2").mouseleave(function(){
+				$("#ln2").css('background-color','#001063 ');
+			});
+			$("#ln3").mouseleave(function(){
+				$("#ln3").css('background-color','#001063 ');
+			});
+			$("#ln4").mouseleave(function(){
+				$("#ln4").css('background-color','#001063 ');
+			});
+			$("#ln5").mouseleave(function(){
+				$("#ln5").css('background-color','#001063 ');
+			});
+			$("#ln6").mouseleave(function(){
+				$("#ln6").css('background-color','#001063 ');
+			});
+			$("#ln7").mouseleave(function(){
+				$("#ln7").css('background-color','#001063 ');
+			});
+			$("#ln8").mouseleave(function(){
+				$("#ln8").css('background-color','#001063 ');
+			});
+			$("#ln9").mouseleave(function(){
+				$("#ln9").css('background-color','#001063 ');
+			});
+			$("#ln10").mouseleave(function(){
+				$("#ln10").css('background-color','#001063 ');
+			});
+
+			$("#ln11").hover(function(){
+				$("#ln11").css('background-color','#D70505');
+			});
+			$("#ln12").hover(function(){
+				$("#ln12").css('background-color','#D70505');
+			});
+			$("#ln13").hover(function(){
+				$("#ln13").css('background-color','#D70505');
+			});
+			$("#ln14").hover(function(){
+				$("#ln14").css('background-color','#D70505');
+			});
+			$("#ln15").hover(function(){
+				$("#ln15").css('background-color','#D70505');
+			});
+			$("#ln16").hover(function(){
+				$("#ln16").css('background-color','#D70505');
+			});
+			$("#ln17").hover(function(){
+				$("#ln17").css('background-color','#D70505');
+			});
+			$("#ln18").hover(function(){
+				$("#ln18").css('background-color','#D70505');
+			});
+			$("#ln19").hover(function(){
+				$("#ln19").css('background-color','#D70505');
+			});
+			$("#ln20").hover(function(){
+				$("#ln20").css('background-color','#D70505');
+			});
+
+			$("#ln11").mouseleave(function(){
+				$("#ln11").css('background-color','#001063 ');
+			});
+			$("#ln12").mouseleave(function(){
+				$("#ln12").css('background-color','#001063 ');
+			});
+			$("#ln13").mouseleave(function(){
+				$("#ln13").css('background-color','#001063 ');
+			});
+			$("#ln14").mouseleave(function(){
+				$("#ln14").css('background-color','#001063 ');
+			});
+			$("#ln15").mouseleave(function(){
+				$("#ln15").css('background-color','#001063 ');
+			});
+			$("#ln16").mouseleave(function(){
+				$("#ln16").css('background-color','#001063 ');
+			});
+			$("#ln17").mouseleave(function(){
+				$("#ln17").css('background-color','#001063 ');
+			});
+			$("#ln18").mouseleave(function(){
+				$("#ln18").css('background-color','#001063 ');
+			});
+			$("#ln19").mouseleave(function(){
+				$("#ln19").css('background-color','#001063 ');
+			});
+			$("#ln20").mouseleave(function(){
+				$("#ln20").css('background-color','#001063 ');
+			});
+
+			$("#ln21").hover(function(){
+				$("#ln21").css('background-color','#D70505');
+			});
+			$("#ln22").hover(function(){
+				$("#ln22").css('background-color','#D70505');
+			});
+			$("#ln23").hover(function(){
+				$("#ln23").css('background-color','#D70505');
+			});
+			$("#ln24").hover(function(){
+				$("#ln24").css('background-color','#D70505');
+			});
+			$("#ln25").hover(function(){
+				$("#ln25").css('background-color','#D70505');
+			});
+			$("#ln26").hover(function(){
+				$("#ln26").css('background-color','#D70505');
+			});
+			$("#ln27").hover(function(){
+				$("#ln27").css('background-color','#D70505');
+			});
+			$("#ln28").hover(function(){
+				$("#ln28").css('background-color','#D70505');
+			});
+			$("#ln29").hover(function(){
+				$("#ln29").css('background-color','#D70505');
+			});
+			$("#ln30").hover(function(){
+				$("#ln30").css('background-color','#D70505');
+			});
+			$("#ln31").hover(function(){
+				$("#ln31").css('background-color','#D70505');
+			});
+			$("#ln32").hover(function(){
+				$("#ln32").css('background-color','#D70505');
+			});
+
+			$("#ln21").mouseleave(function(){
+				$("#ln21").css('background-color','#001063 ');
+			});
+			$("#ln22").mouseleave(function(){
+				$("#ln22").css('background-color','#001063 ');
+			});
+			$("#ln23").mouseleave(function(){
+				$("#ln23").css('background-color','#001063 ');
+			});
+			$("#ln24").mouseleave(function(){
+				$("#ln24").css('background-color','#001063 ');
+			});
+			$("#ln25").mouseleave(function(){
+				$("#ln25").css('background-color','#001063 ');
+			});
+			$("#ln26").mouseleave(function(){
+				$("#ln26").css('background-color','#001063 ');
+			});
+			$("#ln27").mouseleave(function(){
+				$("#ln27").css('background-color','#001063 ');
+			});
+			$("#ln28").mouseleave(function(){
+				$("#ln28").css('background-color','#001063 ');
+			});
+			$("#ln29").mouseleave(function(){
+				$("#ln29").css('background-color','#001063 ');
+			});
+			$("#ln30").mouseleave(function(){
+				$("#ln30").css('background-color','#001063 ');
+			});
+			$("#ln31").mouseleave(function(){
+				$("#ln31").css('background-color','#001063 ');
+			});
+			$("#ln32").mouseleave(function(){
+				$("#ln32").css('background-color','#001063 ');
+			});
+			$("#programa").hover(function(){
+				$("#programa").css('background-color', '#979696');
+				$("#programa").css('color', '#000000');
+			});
+			$("#programa").mouseleave(function(){
+				$("#programa").css('background-color', '#BE1810');
+				$("#programa").css('color', '#fff');
+			});
+			var i = 1;
+
+			$("#imagen").mouseover(function(){
+			
+				setTimeout(this.src = "images/" + i + ".jpg", 1000);
+				console.log(i);
+				i++;
+				
+				if (i == 7){
+					i = 1;
+				}
+		
+			});
+
+		
+
+});
